fef08d3947ea0ba10361d2a301b3a6bff547ea88
Vladimir <vladimirzq@Gmail.com>
2020-06-14 18:17:02 +0600

    added tabs functionality

---
+++ src/js/slider-1.js
@@ -0,0 +1,56 @@
+export const sliderFirstPage = () => {
+    let slideIndex = 1,
+        slides = document.querySelectorAll(".promo__slider-item"),
+        prev = document.querySelector(".promo__slider-prev"),
+        next = document.querySelector(".promo__slider-next"),
+        dotsWrap = document.querySelector(".promo__slider-dots"),
+        dots = document.querySelectorAll(".promo__slider-dot");
+
+    showSlides(slideIndex);
+    // setInterval(() => plusSlides(1), 10000);
+
+    function showSlides(n) {
+    //главная функция
+    if (n > slides.length) {
+        //зацикливаем слайды
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    slides.forEach((item) => (item.style.display = "none")); //скрываем все слайды изначально
+    dots.forEach((item) => item.classList.remove("dot-active")); //делаем неактивными все точки изначально
+
+    slides[slideIndex - 1].style.display = "flex"; //показываем первый слайд
+    dots[slideIndex - 1].classList.add("dot-active"); //делаем активной первую точку
+    }
+
+    function plusSlides(n) {
+    // для того чтобы юзать её в prev и next buttons logic, плюсовать/вычитать
+    showSlides((slideIndex += n));
+    }
+
+    function currentSlide(n) {
+    //для того чтобы юзать её в логике для кликов на определнную dot
+    showSlides((slideIndex = n));
+    }
+
+    prev.addEventListener("click", () => {
+    //prev button logic
+    plusSlides(-1);
+    });
+    next.addEventListener("click", () => {
+    //next button logic
+    plusSlides(1);
+    });
+
+    dotsWrap.addEventListener("click", (event) => {
+    //логикa для кликов на определнную dot
+    for (let i = 0; i < dots.length + 1; i++) {
+        if (event.target.classList.contains("promo__slider-dot") && event.target == dots[i - 1]) {
+        currentSlide(i);
+        }
+    }
+    });
+}
---
+++ src/js/tabs.js
@@ -0,0 +1,48 @@
+export const tabs = () => {
+    let tabsTriggers = document.querySelector(".decisions__tabs-triggers"),
+    tabsTriggersItems = document.querySelectorAll(".decisions__tabs-item"),
+    tabsContentItems = document.querySelectorAll(".decisions__tabs-content-item"),
+    tabsArrows = document.querySelector('.decisions__tabs-arrows'),
+    activeTab = 0,
+    countOfTabs = tabsTriggersItems.length;
+
+    tabsTriggers.addEventListener("click", function(event) {
+        const target = event.target;
+        event.preventDefault();
+
+        if(target.classList.contains('decisions__tabs-item') || 
+            target.parentNode.classList.contains('decisions__tabs-item')){
+                console.log('hello');
+                
+                tabsTriggersItems.forEach(el => el.classList.remove("decisions__tabs-item--active"))
+                tabsContentItems.forEach(el => el.classList.remove("decisions__tabs-content-item--active"))
+                target.classList.add("decisions__tabs-item--active");
+        
+                const idContentToShow = target.getAttribute('href');
+                document.querySelector(idContentToShow).classList.add('decisions__tabs-content-item--active');
+            }
+    });
+    tabsTriggersItems[0].click()
+
+    tabsArrows.addEventListener('click', function(event) {
+        const target = event.target;
+        event.preventDefault();
+        
+        if(target && (target.classList.contains('decisions__tabs-arrows-left') ||
+            target.parentNode.classList.contains('decisions__tabs-arrows-left'))){
+                tabsTriggersItems.forEach((el, idx) => el.className.includes('--active') ? activeTab = idx - 1 : '')
+                if(activeTab < 0) activeTab = countOfTabs - 1;
+                if(activeTab > countOfTabs - 1) activeTab = 0;
+
+                tabsTriggersItems[activeTab].click();
+            
+        } else if ((target && (target.classList.contains('decisions__tabs-arrows-right') ||
+                    target.parentNode.classList.contains('decisions__tabs-arrows-right')))){
+            tabsTriggersItems.forEach((el, idx) => el.className.includes('--active') ? activeTab = idx + 1 : '')
+            if(activeTab < 0) activeTab = countOfTabs - 1;
+            if(activeTab > countOfTabs - 1) activeTab = 0;
+
+            tabsTriggersItems[activeTab].click();
+        }
+    })
+}
